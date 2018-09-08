c2b37882fcd96a9dd1f7e4f0fd07e0a8a921b702
candy-wei <379074095@qq.com>
2018-09-08 17:15:35 +0800

    做了一些删除，不要在意

--- emptyProject/first/src/main/java/cn/itcast/dao/impl/StudentDao.java
+++ emptyProject/first/src/main/java/cn/itcast/dao/impl/StudentDao.java
@@ -10,14 +10,10 @@ import cn.itcast.pojo.Student;
  */
 public class StudentDao implements Dao<Student> {
 
-    public void save(Student t) {
-        System.out.println("保存同学");
-    }
-
     public void delete(Student t) {
         System.out.println("删除一个学生");
     }
-
+    ／／这是一个标记
     public void update(Student t) {
 
         System.out.println("更新学生");
